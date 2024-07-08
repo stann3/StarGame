35d4507cb1c0cdeef57513cab5d6c1011c23b9c6
stann3 <123800158+stann3@users.noreply.github.com>
2024-07-08 12:17:59 +0300

    Add files via upload

---
+++ Game/bullet.py
@@ -0,0 +1,17 @@
+import pygame
+from settings import white
+
+class Bullet(pygame.sprite.Sprite):
+    def __init__(self, x, y):
+        super().__init__()
+        self.image = pygame.Surface((10, 20))
+        self.image.fill(white)
+        self.rect = self.image.get_rect()
+        self.rect.bottom = y
+        self.rect.centerx = x
+        self.speed_y = -10
+
+    def update(self):
+        self.rect.y += self.speed_y
+        if self.rect.bottom < 0:
+            self.kill()
